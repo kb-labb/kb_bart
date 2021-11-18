96d37d1d36fac761d0b7f5d236913c03e19a53f1
Faton Rekathati <eufatonr@vglogin0008.vega.izum.si>
2021-11-18 09:38:01 +0100

    minor changes

--- sentencepiece_encoder.py
+++ sentencepiece_encoder.py
@@ -6,7 +6,15 @@ sp = spm.SentencePieceProcessor(model_file="spm.bpe.model",)  # add_bos=True, ad
 sp.encode("Det här är en överdriven testmening", out_type=str)
 sp.encode("<s> Testa vad som sker med BOS- och EOS-symboler</s>", out_type=str)
 
-with open("output00", "r") as rf, open("file00.bpe", "w") as wf:
+
+with open("oscar_train.txt", "r") as rf, open("oscar_train.bpe", "w") as wf:
     for line in rf:
         wf.write(" ".join(sp.encode(line, out_type=str)))
         wf.write("\n")
+
+with open("oscar_valid.txt", "r") as rf, open("oscar_valid.bpe", "w") as wf:
+    for line in rf:
+        wf.write(" ".join(sp.encode(line, out_type=str)))
+        wf.write("\n")
+
+print("Done.")
--- sentencepiece_trainer.py
+++ sentencepiece_trainer.py
@@ -1,7 +1,8 @@
 import sentencepiece as spm
-from datasets import load_dataset
 
-dataset = load_dataset("oscar", "unshuffled_deduplicated_sv")
+# from datasets import load_dataset
+
+# dataset = load_dataset("oscar", "unshuffled_deduplicated_sv", cache_dir="/ceph/hpc/home/eufatonr/faton/kb_bart/oscar")
 
 
 def batch_iterator(dataset, dataset_size, batch_size):
@@ -14,20 +15,23 @@ def batch_iterator(dataset, dataset_size, batch_size):
         yield list(text_batch)
 
 
-def create_txt_from_dataset(dataset, text_line_generator):
-    with open("output.txt", "w") as f:
+def create_txt_from_dataset(text_line_generator, filename):
+    with open(filename, "w") as f:
         for line in text_line_generator:
             f.writelines(line)
 
 
-text_line_generator = batch_iterator(dataset["train"], 11000000, 50)
-create_txt_from_dataset(dataset["train"], text_line_generator)
+# text_line_generator = batch_iterator(dataset["train"], len(dataset["train"]), 50)
+# create_txt_from_dataset(text_line_generator, "oscar_train.txt")
 
 spm.SentencePieceTrainer.train(
-    input="output.txt",
+    input="oscar_train.txt",
     model_prefix="spm.bpe",
-    vocab_size=50260,
+    vocab_size=50265,
     user_defined_symbols=["<mask>"],
     model_type="bpe",
+    bos_id=0,
+    pad_id=1,
+    eos_id=2,
+    unk_id=3,
 )
-
