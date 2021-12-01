375bbc17014d04f94db3159abe053a3c75b84b59
Faton Rekathati <eufatonr@vglogin0007.vega.izum.si>
2021-12-01 15:00:30 +0100

    Chane id of <mask> token to 50184 in added_tokens

--- fix_tokenizer.py
+++ fix_tokenizer.py
@@ -16,22 +16,39 @@ def fix_tokenizer(old_tokenizer, new_vocab: Dict[str, int]):
     new_tokenizer["model"]["vocab"] = deepcopy(new_vocab)
     for k in old_tokenizer["model"]["vocab"]:
         if k not in new_vocab:
-            new_tokenizer["model"]["vocab"][k] = len(
-                new_tokenizer["model"]["vocab"])
+            new_tokenizer["model"]["vocab"][k] = len(new_tokenizer["model"]["vocab"])
+
+    # Change <mask> token id from 4 to 50184
+    new_tokenizer["added_tokens"][4]["id"] = 50184
     return new_tokenizer
 
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tokenizer", type=str, default="tokenizer.json",
-                        help="Path to the huggingface tokenizer-json")
-    parser.add_argument("--checkpoint",
-                        type=str,
-                        default="checkpoint_best.pt", help="Name of the BART checkpoint file.")
-    parser.add_argument("--folder", type=str, default="bart_model",
-                        help="Path to the folder containing a BART checkpoint and dict.txt")
-    parser.add_argument("--new_tokenizer", type=str,
-                        default="tokenizer_fixed.json", help="Path to the new tokenizer-json")
+    parser.add_argument(
+        "--tokenizer",
+        type=str,
+        default="tokenizer.json",
+        help="Path to the huggingface tokenizer-json",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default="checkpoint_best.pt",
+        help="Name of the BART checkpoint file.",
+    )
+    parser.add_argument(
+        "--folder",
+        type=str,
+        default="bart_model",
+        help="Path to the folder containing a BART checkpoint and dict.txt",
+    )
+    parser.add_argument(
+        "--new_tokenizer",
+        type=str,
+        default="tokenizer_fixed.json",
+        help="Path to the new tokenizer-json",
+    )
     return parser.parse_args()
 
 
@@ -42,15 +59,14 @@ def main():
     with open(args.tokenizer) as fin:
         tokenizer = json.load(fin)
 
-    bart = BARTModel.from_pretrained(
-        args.folder, checkpoint_file=args.checkpoint)
+    bart = BARTModel.from_pretrained(args.folder, checkpoint_file=args.checkpoint)
     model_dict = bart.task.source_dictionary.__dict__
     new_vocab = model_dict["indices"]
 
     fixed_tokenizer = fix_tokenizer(tokenizer, new_vocab)
 
     with open(args.new_tokenizer, "w") as fout:
-        json.dump(fixed_tokenizer, fout)
+        json.dump(fixed_tokenizer, fout, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
